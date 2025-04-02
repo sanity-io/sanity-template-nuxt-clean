43da4e597776d590260810cd2ccd0283f443e667
KenJonesPizza <kenjwho@gmail.com>
2025-04-02 15:36:27 -0500

    Add api version fallback

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
   sanity: {
     projectId: process.env.NUXT_SANITY_PROJECT_ID,
     dataset: process.env.NUXT_SANITY_DATASET,
-    apiVersion: process.env.NUXT_SANITY_API_VERSION,
+    apiVersion: process.env.NUXT_SANITY_API_VERSION || "2025-04-01",
     token: process.env.NUXT_SANITY_API_READ_TOKEN, // Only required when using a private dataset
     visualEditing: {
       token: process.env.NUXT_SANITY_API_READ_TOKEN,
